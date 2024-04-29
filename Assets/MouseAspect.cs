2c3073f6da442c65f9976a2a054ce677a7326f00
loup <loup.pdp@orange.fr>
2024-04-29 21:48:12 +0200

    changement sur curseur

---
+++ Assets/MouseAspect.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class MouseAspect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Texture2D cursorTexture;
+    public Texture2D DefaultTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot= Vector2.zero;
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorTexture,hotSpot,cursorMode);
+    }
+
+    // Update is called once per frame
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(DefaultTexture,Vector2.zero,cursorMode);
+    }
+}
