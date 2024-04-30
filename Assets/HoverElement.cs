d3e71a3ede57ac7692bed3df68bc2506ac86c33b
loup <loup.pdp@orange.fr>
2024-04-30 12:15:56 +0200

    start cursor from first

--- Assets/HoverElement.cs
+++ Assets/HoverElement.cs
@@ -8,18 +8,9 @@ public class HoverElement : MonoBehaviour
     public Texture2D DefaultTexture;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot= Vector2.zero;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void changeWhenHover(){
+        
         Cursor.SetCursor(cursorTexture,hotSpot,cursorMode);
     }
     public void changeWhenLeaves(){
