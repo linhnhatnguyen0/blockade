aaa62417a0fd9387bdc121878495680ed2148774
lusjedji <ludivine.michaud3@gmail.com>
2024-05-26 21:04:09 +0200

    ajout ia

---
+++ Assets/Classes/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+Graphe g = new Graphe();
+Console.WriteLine(g.ExisteChemin(g.Sommets[36], g.Sommets[102]));
+List<Sommet> list = g.Dijkstra(g.Sommets[36], g.Sommets[102]);
+foreach(Sommet s in list)
+{
+    Console.WriteLine(s.X + " ; " + s.Y);
+}
+
+
