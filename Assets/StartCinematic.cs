6f58191708808dbd7ddbea6e6c8260d5c90d9f2c
Abderrazak MESSAOUDI <abderrazak.messaoudi@universite-paris-saclay.fr>
2024-06-15 21:06:01 +0200

    Finalisation du script du mouvement de la caméra et de son effet sonore

--- Assets/StartCinematic.cs
+++ Assets/StartCinematic.cs
@@ -4,68 +4,51 @@ using UnityEngine;
 
 public class StartCinematic : MonoBehaviour
 {
-
-    public GameObject target; // Le GameObject autour duquel la caméra va tourner
-    public float rotationSpeed = 30f; // Vitesse de rotation (en degrés par seconde)
-    public float duration = 6f; // Durée d'un tour complet (en secondes)
-    private Quaternion initialRotation; // Rotation initiale de la caméra
-
-    private Vector3 initialPosition; // Position initiale de la caméra
-
-    private float elapsedTime = 0f; // Temps écoulé depuis le début du mouvement
+    public GameObject target; 
+    private Quaternion initialRotation; 
+    private Vector3 initialPosition; 
+    private float elapsedTime = 0f; 
     private Vector3 board;
-
     private bool startCinematic = false; // Flag pour démarrer la cinématique
-
     public GameObject interfacejeu;
-
+    public AudioSource audioSource; // Référence à l'AudioSource
+    public AudioClip cameraSoundClip; // Référence au clip audio pour la phase 1
 
     void Start()
     {
         // Cacher l'interface de jeu
         interfacejeu.SetActive(false);
-        // Enregistrez la rotation initiale de la caméra
         initialRotation = transform.rotation;
-        // Enregistrez la position initiale de la caméra
         initialPosition = transform.position;
-
         board = GameObject.Find("Board").transform.position;
+
     }
 
     void Update()
     {
         if (startCinematic)
         {
-            // Assurez-vous que le GameObject cible est défini
             if (target != null)
             {
-                // Calculer le nombre de tours complets effectués
-                float completedTurns = elapsedTime / duration;
+                elapsedTime += Time.deltaTime;
 
-                // Vérifier si 3 tours complets ont été effectués
-                if (completedTurns >= 1f)
+                float rotationSpeed = (elapsedTime <= 1.5f) ? 15f : 60f;
+
+                transform.RotateAround(board, board, rotationSpeed * Time.deltaTime);
+
+                transform.LookAt(target.transform);
+
+                // Réinitialisation à la fin de l'animation
+                if (elapsedTime >= 7.5f)
                 {
-                    // Revenir à la rotation initiale
                     transform.rotation = initialRotation;
-                    // Revenir à la position initiale
                     transform.position = initialPosition;
+                    audioSource.Stop(); // Arrêter le son à la fin de l'animation
                     // Arrêter le mouvement de la caméra
                     startCinematic = false;
                     interfacejeu.SetActive(true); // Afficher l'interface de jeu
                     return;
                 }
-
-                // Rotation autour du centre du GameObject cible (horizontalement)
-                transform.RotateAround(board, board, rotationSpeed * Time.deltaTime);
-
-                // Gardez la caméra tournée vers le GameObject cible
-                transform.LookAt(target.transform);
-
-                // Mettre à jour le temps écoulé
-                elapsedTime += Time.deltaTime;
-
-                Debug.Log(elapsedTime);
-                
             }
         }
     }
@@ -77,7 +60,7 @@ public class StartCinematic : MonoBehaviour
         elapsedTime = 0f;
         // Cacher l'interface de jeu au début de la cinématique
         interfacejeu.SetActive(false);
+        audioSource.clip = cameraSoundClip;
+        audioSource.Play();
     }
 }
-
-
